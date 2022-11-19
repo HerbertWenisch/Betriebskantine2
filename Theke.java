06026b395b0d457e9e816d078a8da2f68c932021
HerbertWenisch <herbert.wenisch@t-online.de>
2022-11-19 20:00:59 +0100

    "Solution with wait() and notifyAll()"

--- Theke.java
+++ Theke.java
@@ -9,14 +9,37 @@ public class Theke {
     private final int N = 10;
     private Tablett[] tabletts = new Tablett[N];
     private int count = 0;  // erster freier Platz im Array
+    
+    private boolean isFull(){
+        return count == N;
+    }
 
-    public boolean ablegen(Tablett t){
-        return true;
+    private boolean isEmpty(){
+        return count == 0;
+    }
+
+    
+    public synchronized void ablegen(Tablett t){
+        while(isFull()){ 
+          try {wait();}
+          catch (InterruptedException e) {}
+        } 
+        
+        // not full:
+        tabletts[count++] = t;
+        if (count == 1) notifyAll(); // zuvor leer
     }
     
 
-    public Tablett wegnehmen(){
-        return tabletts[0];
+    public synchronized Tablett wegnehmen(){
+        while(isEmpty()){
+           try {wait();}
+           catch (InterruptedException e) {} 
+        }
+        // not empty:
+        Tablett tablett = tabletts[--count];
+        if(count == N-1) notifyAll(); // zuvor voll
+        return tablett;
     }
     
     
