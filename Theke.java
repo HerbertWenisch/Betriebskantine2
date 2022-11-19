c48c3ba7fca69236c7c01ff2d961cba32c71ca73
HerbertWenisch <herbert.wenisch@t-online.de>
2022-11-19 19:28:53 +0100

    "Exercise version: please complete!"

--- Theke.java
+++ Theke.java
@@ -10,26 +10,13 @@ public class Theke {
     private Tablett[] tabletts = new Tablett[N];
     private int count = 0;  // erster freier Platz im Array
 
-    private boolean isFull(){
-        return count == N;
-    }
-    
-    private boolean isEmpty(){
-        return count == 0;
-    }
-
-    public synchronized boolean ablegen(Tablett t){
-        if(isFull())return false;
-        // not full:
-        tabletts[count++]= t;
+    public boolean ablegen(Tablett t){
         return true;
     }
     
 
-    public synchronized Tablett wegnehmen(){
-        if(isEmpty()) return null;
-        // not empty:
-        return tabletts[--count];
+    public Tablett wegnehmen(){
+        return tabletts[0];
     }
     
     
